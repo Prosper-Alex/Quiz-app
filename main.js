8ac01ee8b6685304d64c7b2ad3d66832c42589ae
Prosper Alex <chigoziealex011@gmail.com>
2025-11-18 07:37:01 +0100

    Refactor code for consistency in string quotes and improve readability

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const questionTextEl = document.getElementById('question');
-const answerButtonsEl = document.getElementById('answer-buttons');
-const startBtnEl = document.getElementById('start-btn');
-const prevBtn = document.getElementById('prev-btn');
-const nextQuestionEl = document.getElementById('next-btn');
-const feedbackEl = document.getElementById('feedback');
-const restartBtn = document.getElementById('restart-btn');
-const startScreenEl = document.getElementById('start-screen');
-const quizContainerEl = document.getElementById('quiz-container');
-const resultScreenEl = document.getElementById('result-screen');
-const resultEl = document.getElementById('result');
+const questionTextEl = document.getElementById("question");
+const answerButtonsEl = document.getElementById("answer-buttons");
+const startBtnEl = document.getElementById("start-btn");
+const prevBtn = document.getElementById("prev-btn");
+const nextQuestionEl = document.getElementById("next-btn");
+const feedbackEl = document.getElementById("feedback");
+const restartBtn = document.getElementById("restart-btn");
+const startScreenEl = document.getElementById("start-screen");
+const quizContainerEl = document.getElementById("quiz-container");
+const resultScreenEl = document.getElementById("result-screen");
+const resultEl = document.getElementById("result");
 
 const state = {
   currentQuestionIndex: 0,
@@ -17,7 +17,7 @@ const state = {
 };
 
 async function getQuestions() {
-  const TOKEN = 'QIWJoDjZKgdAGDrueDFiosRVh77gegkTFlq1KufH';
+  const TOKEN = "LaMCPmyqzidHhv6CQOunBMcBMQSqx4SOHE3Ze3jE";
   const url = `https://quizapi.io/api/v1/questions?apiKey=${TOKEN}&limit=10`;
 
   const response = await fetch(url);
@@ -26,7 +26,7 @@ async function getQuestions() {
   return data.map((q) => {
     const options = Object.values(q.answers).filter((o) => o !== null);
     const correct = Object.values(q.correct_answers).findIndex(
-      (o) => o === 'true'
+      (o) => o === "true"
     );
 
     return {
@@ -40,11 +40,11 @@ async function getQuestions() {
 function renderQuestion(question) {
   questionTextEl.innerHTML = question.question;
 
-  answerButtonsEl.innerHTML = '';
+  answerButtonsEl.innerHTML = "";
 
   question.options.forEach((option, index) => {
     answerButtonsEl.insertAdjacentHTML(
-      'beforeend',
+      "beforeend",
       `<button class="btn" data-index="${index}">${option}</button>`
     );
   });
@@ -52,22 +52,22 @@ function renderQuestion(question) {
 
 async function startQuiz() {
   try {
-    startBtnEl.textContent = 'Loading...';
-    startBtnEl.setAttribute('disabled', true);
+    startBtnEl.textContent = "Loading...";
+    startBtnEl.setAttribute("disabled", true);
 
     const questions = await getQuestions();
     state.quizData = questions;
 
-    startScreenEl.classList.add('hidden');
-    resultScreenEl.classList.add('hidden');
-    quizContainerEl.classList.remove('hidden');
+    startScreenEl.classList.add("hidden");
+    resultScreenEl.classList.add("hidden");
+    quizContainerEl.classList.remove("hidden");
 
     renderQuestion(state.quizData[state.currentQuestionIndex]);
   } catch (error) {
     console.log(errror);
   } finally {
-    startBtnEl.textContent = 'Start Quiz';
-    startBtnEl.removeAttribute('disabled');
+    startBtnEl.textContent = "Start Quiz";
+    startBtnEl.removeAttribute("disabled");
   }
 }
 
@@ -75,7 +75,7 @@ function prevQuestion() {
   if (state.currentQuestionIndex > 0) {
     state.currentQuestionIndex--;
     renderQuestion(state.quizData[state.currentQuestionIndex]);
-    feedbackEl.classList.add('hidden');
+    feedbackEl.classList.add("hidden");
   }
 }
 
@@ -83,28 +83,28 @@ function nextQuestion() {
   if (state.currentQuestionIndex < quizData.length - 1) {
     state.currentQuestionIndex++;
     renderQuestion(state.quizData[state.currentQuestionIndex]);
-    feedbackEl.classList.add('hidden');
+    feedbackEl.classList.add("hidden");
   }
 }
 
 function answerQuestion(answerIndex) {
-  feedbackEl.classList.remove('hidden');
+  feedbackEl.classList.remove("hidden");
 
   const correctIndex = state.quizData[state.currentQuestionIndex].correct;
 
-  document.querySelectorAll('#answer-buttons .btn').forEach((btnEl) => {
+  document.querySelectorAll("#answer-buttons .btn").forEach((btnEl) => {
     const valid = +btnEl.dataset.index === correctIndex;
-    valid ? btnEl.classList.add('correct') : btnEl.classList.add('wrong');
+    valid ? btnEl.classList.add("correct") : btnEl.classList.add("wrong");
   });
 
   if (+answerIndex === correctIndex) {
     state.score++;
-    feedbackEl.classList.add('correct');
-    feedbackEl.classList.remove('wrong');
+    feedbackEl.classList.add("correct");
+    feedbackEl.classList.remove("wrong");
     feedbackEl.innerHTML = "You're correct!";
   } else {
-    feedbackEl.classList.add('wrong');
-    feedbackEl.classList.remove('correct');
+    feedbackEl.classList.add("wrong");
+    feedbackEl.classList.remove("correct");
     feedbackEl.innerHTML = "You're wrong!";
   }
 
@@ -114,8 +114,8 @@ function answerQuestion(answerIndex) {
 }
 
 function showResults() {
-  quizContainerEl.classList.add('hidden');
-  resultScreenEl.classList.remove('hidden');
+  quizContainerEl.classList.add("hidden");
+  resultScreenEl.classList.remove("hidden");
 
   const total = state.quizData.length;
   const percentage = (state.score / total) * 100;
@@ -127,20 +127,20 @@ function showResults() {
   }
 }
 
-restartBtn.addEventListener('click', () => {
+restartBtn.addEventListener("click", () => {
   state.currentQuestionIndex = 0;
   state.score = 0;
-  resultScreenEl.classList.add('hidden');
-  quizContainerEl.classList.remove('hidden');
-  startQuiz()
+  resultScreenEl.classList.add("hidden");
+  quizContainerEl.classList.remove("hidden");
+  startQuiz();
   // renderQuestion(state.quizData[state.currentQuestionIndex]);
 });
 
-startBtnEl.addEventListener('click', startQuiz);
-prevBtn.addEventListener('click', prevQuestion);
-nextQuestionEl.addEventListener('click', nextQuestion);
-answerButtonsEl.addEventListener('click', (event) => {
-  if (event.target.matches('.btn')) {
+startBtnEl.addEventListener("click", startQuiz);
+prevBtn.addEventListener("click", prevQuestion);
+nextQuestionEl.addEventListener("click", nextQuestion);
+answerButtonsEl.addEventListener("click", (event) => {
+  if (event.target.matches(".btn")) {
     answerQuestion(event.target.dataset.index);
   }
 });
